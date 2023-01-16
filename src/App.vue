891601934a06bef11a0be805c2ecd81160c2fca0
Fededada23 <dadamofederico@gmail.com>
2023-01-16 14:52:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios'
+import AppHeader from './components/AppHeader.vue';
+import Select from './components/Select.vue';
+import CharactersList from './components/CharactersList.vue';
+import { store } from './store.js';
+export default {
+  components: {
+    AppHeader,
+    CharactersList,
+    Select,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  created() {
+    this.getCharacters();
+  },
+  methods: {
+    getCharacters() {
+      axios.get(store.url).then((response) => {
+        store.charactersList = response.data.data
+        setTimeout(() => {
+          store.loading = false
+        }, 3000)
+      })
+    }
+  }
+}
+</script>
+<template lang="">
+  <div class="container-yugi">
+    <AppHeader message="Yu-Gi-Oh App"/>
+    <Select></Select> 
+    <main>
+      <CharactersList />
+    </main>
+  </div>
+</template>
+<style lang="scss">
+@use './styles/generals.scss' as *;
+</style>
